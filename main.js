c17a1361c75d591113ecb6b22a08da91ad2f1496
yanglei302 <yanglei302@jd.com>
2022-03-26 12:05:30 +0800

    update

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+window.onerror = (e)=>{
+  console.log('1111', e)
+}
 new Promise(resolve=>{
   console.log(a)
-}).catch((e)=>{
-  console.log('catch', e)
 })
+// .catch((e)=>{
+//   // console.log(e)
+// })
 
 window.addEventListener('unhandledrejection', e=>{
   let {error, promise} = e
   console.log(e)
 })
+
+console.log(b)
+// throw new Error("b")
+console.log('ok')
+
